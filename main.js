a503c2dcbaa656e16dfc97857bbf61842ea7a279
fatmamath <massoufatima23@gmail.com>
2024-01-27 00:39:38 +0100

    mini projet with json .

---
+++ main.js
@@ -0,0 +1,21 @@
+const list = []
+
+function add() {
+    list.push(document.getElementById("input-tap").value + " ")
+    document.getElementById("pforstoke").innerHTML = list
+    document.getElementById("input-tap").value = ''
+}
+
+function random(){
+    document.getElementById("input-tap").value = ''
+    const random = Math.floor( Math.random() * list.length)
+    document.getElementById("pforstoke").innerHTML = list[random]
+
+}
+function Remove(){
+    document.getElementById("pforstoke").innerHTML = list
+    while (list.length > 0){
+        list.pop()
+    }
+}   
+
