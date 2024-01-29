94afb5ded18445610e4748f84ee2a606c918a969
fatmamath <massoufatima23@gmail.com>
2024-01-29 11:18:51 +0100

    mini projet with json .

--- main.js
+++ main.js
@@ -10,12 +10,11 @@ function random(){
     document.getElementById("input-tap").value = ''
     const random = Math.floor( Math.random() * list.length)
     document.getElementById("pforstoke").innerHTML = list[random]
-
 }
 function Remove(){
-    document.getElementById("pforstoke").innerHTML = list
     while (list.length > 0){
         list.pop()
     }
+    document.getElementById("pforstoke").innerHTML = list
 }   
 
